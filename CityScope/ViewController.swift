b88a650599f6f76e80c4ed2d445db62bf3e2495a
vsukiasyan <vsukiasyan1@hotmail.com>
2018-04-27 17:21:51 -0700

    Initial Commit

---
+++ CityScope/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CityScope
+//
+//  Created by Vic Sukiasyan on 4/27/18.
+//  Copyright © 2018 Vic Sukiasyan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
